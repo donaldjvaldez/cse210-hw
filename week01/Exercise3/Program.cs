a3eaf72ef0748a1ce106bc7e1dec1395e1b9ba27
donaldjvaldez <donaldj.valdez@gmail.com>
2025-09-06 18:45:26 -0400

    Exercise 3 done (still struggling with indentation)

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            do
+            {
+                Console.Write("What is your guess? (between 1-100) ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
