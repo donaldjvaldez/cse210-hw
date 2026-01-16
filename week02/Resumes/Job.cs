0f4a4c7cb500c3965e33b008bc49204490f7d6f2
donaldjvaldez <donaldj.valdez@gmail.com>
2026-01-16 10:47:51 -0400

    Learning activity w02 done

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -3,4 +3,8 @@ public class Job {
     public string _jobTitle;
     public int _startYear;
     public int _endYear;
+
+    public void Display() {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}─{_endYear}");
+    }
 }
