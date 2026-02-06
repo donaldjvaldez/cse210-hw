628b48226d9d5ed68ad7429a301dfea104b4c558
donaldjvaldez <donaldj.valdez@gmail.com>
2026-02-06 17:54:32 -0400

    Assignment Done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+       Assignment a1 = new Assignment("Michael Jordan", "Basic Grammar");
+       Console.WriteLine(a1.GetSummary());
+
+       MathAssignment m1 = new MathAssignment("Kyrie Irving", "Fractions", "4.5", "3/8" );
+       Console.WriteLine(m1.GetSummary());
+       Console.WriteLine(m1.GetHomeworkList());
+
+       WritingAssignment w1 = new WritingAssignment("Sabrina Ionesky", "Programming", "Object Oriented Programming");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+
     }
 }
